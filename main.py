eb523c0f906d97ebe167422e2c4f64749cc4de43
calvin-k347 <clark.calvink@gmail.com>
2025-07-05 16:51:55 -0400

    heading parser :D

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re, sys
+import sys
 from parser import Parser
-print("hello world")
-
 for arg in sys.argv[1:]:
     p = Parser(arg)
     p.method()
--- parser.py
+++ parser.py
@@ -1,9 +1,23 @@
 import re
 class Parser:
     patterns = {
-        "heading": r"#{1,3}+(.+)",
-        "lines": r"-{3}|\*{3}|_{3}"
+        "heading": re.compile("#{1,3}+(.+)"),
+        "lines": re.compile(r"-{3}|\*{3}|_{3}"),
+        "bold": re.compile(r"\*\*(.+?)\*\*")
     }
+    boiler_plate = '''
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+''' 
     def __init__(self, file):
         self.file = file
         self.content = ""
@@ -12,10 +26,38 @@ class Parser:
             for line in f:
                 self.content += line
     def __parse(self):
-        h = re.findall(self.patterns['heading'], self.content)
+        h = re.findall(Parser.patterns['heading'], self.content)
         l = re.findall(self.patterns['lines'], self.content)
-        print(h)
-        print(l)
+        b = re.findall(self.patterns['bold'], self.content)
+        convert = ""
+        contained = False
+        for i in range(len(self.content)):
+            if not contained:
+                #check if matches heading regex
+                heading = Parser.patterns['heading'].match(self.content[i:])
+                if heading:
+                    depth = heading.group().count("#")
+                    html_heading = f'''<h{depth}>\n{heading.group()[depth:]}\n<h{depth}>
+                    '''
+                    print(html_heading)
+                    contained = True
+                    continue
+                line = Parser.patterns['lines'].match(self.content[i:])
+                if line:
+                    print(line)
+                    contained = True
+                    continue
+                
+
+
+
+        convertion = Parser.boiler_plate[0:190] + convert + Parser.boiler_plate[190:]
+        with open("new.html", "w") as f:
+            pass
+        #print(h)
+        #print(l)
+        #print(b)
+        #print(convertion)
     def method(self):
         self.__read()
         self.__parse()
