b217db24b065cc2318b7feedf14405e55f85a73a
calvin-k347 <clark.calvink@gmail.com>
2025-07-04 20:18:48 -0400

    inital styles

--- parser.py
+++ parser.py
@@ -1,11 +1,21 @@
+import re
 class Parser:
+    patterns = {
+        "heading": r"#{1,3}+(.+)",
+        "lines": r"-{3}|\*{3}|_{3}"
+    }
     def __init__(self, file):
         self.file = file
-        self.contents = []
+        self.content = ""
     def __read(self):
         with open(self.file) as f:
             for line in f:
-                self.contents.append(line)
-        print(self.contents)
+                self.content += line
+    def __parse(self):
+        h = re.findall(self.patterns['heading'], self.content)
+        l = re.findall(self.patterns['lines'], self.content)
+        print(h)
+        print(l)
     def method(self):
         self.__read()
+        self.__parse()
