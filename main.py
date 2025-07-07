de02199770065da52beb057a580373ba0a0d7ebf
calvin-k347 <clark.calvink@gmail.com>
2025-07-07 18:44:26 -0400

    add watcher for changes to inputed files

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import sys
 import subprocess
 from parser import Parser
-for arg in sys.argv[1:]:
-    p = Parser(arg)
-    preview_path = p.method()
-    command = command = ["powershell.exe", "-File","./preview.ps1" ] + [preview_path]
-    subprocess.run(command)
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+
+md_files = []
+def main():
+    for arg in sys.argv[1:]:
+        p = Parser(arg)
+        preview_path = p.method()
+        md_files.append(".\\" + arg)
+        command = command = ["powershell.exe", "-File","./preview.ps1" ] + [preview_path]
+        subprocess.run(command)
+class MardownWatcher(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path in md_files:
+            print(f"Text file modified: {event.src_path}")
+            main()
+if __name__ == "__main__":
+    main()
+    path = "."
+    event_handler = MardownWatcher()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+
+    # Start the observer
+    observer.start()
+    print(f"Monitoring directory: {path}")
+    print(f"target files: {md_files}")
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
--- server.py
+++ server.py
@@ -1,8 +1,31 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+
+class MardownWatcher(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path.endswith(".md"):
+            print(f"Text file modified: {event.src_path}")
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('new.html')
-if __name__ == '__main__':
-   app.run()
+if __name__ == "__main__":
+    path = "."  # Directory to monitor (current directory in this case)
+    event_handler = MardownWatcher()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+
+    # Start the observer
+    observer.start()
+    print(f"Monitoring directory: {path}")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
