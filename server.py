862e8286ecc971b9fc910ff6b80dbdef53144a6c
calvin-k347 <clark.calvink@gmail.com>
2025-07-14 22:36:02 -0400

    add home route

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, Blueprint
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
+@app.route("/")
+def home():
+   return "Hi"
 @app.route('/<int:num>')
-def home(num):
+def previews(num):
    return render_template(f'new{num}.html')
 if __name__ == '__main__':
    app.run(debug=True)
