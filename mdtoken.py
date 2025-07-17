96200e141b417c56a366daf981ac6134305fb7cb
calvin-k347 <clark.calvink@gmail.com>
2025-07-17 00:26:30 -0400

    list valid token types

--- mdtoken.py
+++ mdtoken.py
@@ -1,7 +1,16 @@
 class Token:
     MULTI_LINE_TOKENS = ["list-item"]
+    VALID_TOKENS = {"heading",
+                    "paragraph",
+                    "emphasis", 
+                    "list-item", 
+                    "code", 
+                    "image", 
+                    "line", 
+                    "break",
+                    "multi-line-code"}
     def __init__(self, type, value, level = None):
-        self.type = type
+        self.type = type if type in Token.VALID_TOKENS else "INVALID TOKEN"
         self.value = value
         self.level = level
     def __repr__(self):
